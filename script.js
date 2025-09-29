c113d1d658aeed57b8f721bce207d4ff73ade91c
shubhash1407-sketch <shubhash1407@gmail.com>
2025-09-29 18:43:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("contact-form").addEventListener("submit", function (e) 
   const form = e.target;
   const data = new FormData(form);
 
-  fetch("https://formsubmit.co/sonalsharma660069@gmail.com", {
+  fetch("https://formsubmit.co/rvrbit0@gmail.com", {
     method: "POST",
     body: data,
   })
