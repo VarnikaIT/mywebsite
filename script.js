de8ecf7eb90772431f07bd4787c241ce609c0a37
shubhash1407-sketch <shubhash1407@gmail.com>
2025-09-29 18:18:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const form = e.target;
+  const data = new FormData(form);
+
+  fetch("https://formsubmit.co/sonalsharma660069@gmail.com", {
+    method: "POST",
+    body: data,
+  })
+    .then((res) => {
+      if (res.ok) {
+        document.getElementById("response").innerText = "Message sent!";
+        form.reset();
+      } else {
+        document.getElementById("response").innerText = "Error sending message.";
+      }
+    })
+    .catch(() => {
+      document.getElementById("response").innerText = "Error sending message.";
+    });
+});
